dc9a4fae820360047f3efa6aefe31fdbc0907ec6
LS-2ednar <lukas_schaub@gmx.ch>
2026-04-07 09:29:20 +0200

    created new project and worked on the cli for the movie querying

---
+++ cli/lib/keyword_search.py
@@ -0,0 +1,19 @@
+from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords, text_preprocessing
+from nltk.stem import PorterStemmer
+
+def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
+    movies = load_movies()
+    stopwords = load_stopwords()
+    results = []
+    stemmer = PorterStemmer()
+    for movie in movies:
+        if len(results) >= limit:
+            break
+        movie_token = [stemmer.stem(token) for token in text_preprocessing(movie["title"]) if token not in stopwords]
+        for q_token in [stemmer.stem(token) for token in text_preprocessing(query) if token not in stopwords]:
+            for m_token in movie_token:
+                if q_token in m_token:
+                    if movie not in results:
+                        results.append(movie)
+
+    return results
